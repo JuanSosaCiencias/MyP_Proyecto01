2ded51b825c8556a17e9432e471d534ea5a06230
Gael <gaelchaparro@unam.mx.com>
2023-09-20 22:43:14 -0600

    gael-elimineDivCoordenadas,ValidacionForms

--- Esqueleto-Gael/assets/css/mostrarForms.js
+++ Esqueleto-Gael/assets/css/mostrarForms.js
@@ -2,32 +2,26 @@
 $(document).ready(function () {
 
     $("#entradaTicket").hide();
-    $("#entradaCoordenadas").hide();
     $("#entradaCiudad").show();
 
     /**
-     * Oculta los dos primeros elementos recibidos y muestra en 
-     * pantalla el del 3er parametro
+     * Oculta el elemento correspondiente al primer parametro
+     * recibidos y muestra en pantalla el elemento correspondiente
+     * al 2do elemento
      * @param {*} ocultar1 id de un div a ocultar
-     * @param {*} ocultar2 id de un div a ocultar
      * @param {*} mostrar  id de un div a mostrar
      */
-    function ocultarYMostrarDiv(ocultar1, ocultar2, mostrar) {
-        $(ocultar1).css("display", "none");
-        $(ocultar2).css("display", "none");
+    function ocultarYMostrarDiv(ocultar, mostrar) {
+        $(ocultar).css("display", "none");
         $(mostrar).fadeIn(700);
     }
 
     $("#btnCiudad").click(function () {
-        ocultarYMostrarDiv("#entradaTicket", "#entradaCoordenadas", "#entradaCiudad");
+        ocultarYMostrarDiv("#entradaTicket", "#entradaCiudad");
     });
 
     $("#btnTicket").click(function () {
-        ocultarYMostrarDiv("#entradaCoordenadas", "#entradaCiudad", "#entradaTicket")
-    })
-
-    $("#btnCoordenadas").click(function () {
-        ocultarYMostrarDiv("#entradaTicket", "#entradaCiudad", "#entradaCoordenadas")
+        ocultarYMostrarDiv("#entradaCiudad", "#entradaTicket")
     })
 
 
