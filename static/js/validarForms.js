6d61305074a1a795fede95595cc276395f769f6e
Gael <gaelchaparro@unam.mx.com>
2023-09-22 00:29:09 -0600

    gael-no-permitir-espacios-en-blanco-en-inputs

--- static/js/validarForms.js
+++ static/js/validarForms.js
@@ -18,5 +18,45 @@
             form.classList.add('was-validated')
         }, false)
     })
-})()
+})();
+
+
+$(document).ready(
+    function () {
+
+
+        /**
+         * Codigo sacado de: https://stackoverflow.com/questions/12010275/strip-white-spaces-on-input
+         * Usuario: Kyle
+         * Consultado: 22 de septiembre de 2023
+         * No permite que se pongan espacios de cualquier tipo en el input que se 
+         * recibió como parametro.
+         * @param {*} input el input que disparó el evento
+         */
+        function noEspacios(input) {
+            $(input).val(function (_, v) {
+                return v.replace(/\s+/g, '');
+            });
+        }
+
+        $("#inputCiudadOrigen").on("input", function () {
+            noEspacios("#inputCiudadOrigen");
+        })
+
+        $("#inputCiudadDestino").on("input", function () {
+            noEspacios("#inputCiudadDestino");
+        })
+
+        $("#inputTicket").on("input", function () {
+            noEspacios("#inputTicket");
+        })
+
+    }
+
+);
+
+
+
+
+
 
